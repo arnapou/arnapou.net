cbb2f01dbb5e1a47543023429db1c82a56e455e9
Arnaud Buathier <arnaud@arnapou.net>
2019-05-12 22:35:38 +0000

    change cache folder

--- public/index.php
+++ public/index.php
@@ -5,7 +5,7 @@ include __DIR__ . '/../../site.phar';
 \Cms\Site::run([
     'name'           => 'Arnapou',
     'path_public'    => __DIR__,
-    'path_cache'     => __DIR__ . '/../cache',
+    'path_cache'     => '/cache/arnapou.net',
     'path_templates' => __DIR__ . '/../templates',
     'path_data'      => __DIR__ . '/../data',
     'path_php'       => [
