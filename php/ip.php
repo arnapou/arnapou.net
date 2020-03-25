2d4186f05c9d45c08c0a560ea61ea7f1c183bd8d
Arnaud Buathier <arnaud@buathier.me>
2020-03-25 18:58:29 +0100

    update ip

--- php/ip.php
+++ php/ip.php
@@ -6,46 +6,40 @@ use Symfony\Component\HttpFoundation\Response;
 return new class() extends Controller {
     public function configure(): void
     {
-        $this->addRoute('ip', [$this, 'routeIpTxt'], 'ip');
-        $this->addRoute('ip.{ext}', [$this, 'routeIpHtm'], 'ip')->setRequirement('ext', '(php|htm|html)');
+        $this->addRoute('ip', [$this, 'routeIpTxt'], 'ip-txt');
+        $this->addRoute('ip.{ext}', [$this, 'routeIpHtm'], 'ip-htm')->setRequirement('ext', '(php|htm|html)');
     }
 
-    public function ipDescription($ip, $host)
-    {
-        if (strpos($host, '.mobile.')) {
-            return 'connexion 4G';
-        }
-        if (strpos($host, '.wanadoo.')) {
-            return 'connexion ADSL';
-        }
-        return '';
-    }
-
-    public function routeIpHtm($ext)
+    public function ipData()
     {
         $ip   = $_SERVER['REMOTE_ADDR'];
         $host = gethostbyaddr("$ip");
-        $desc = $this->ipDescription($ip, $host);
-        return new Response("<!DOCTYPE html>
-<html>
-<title>$ip</title>
-<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">
-<style>
-body {
-    text-align:center;
-    font-family: Arial, Helvetica, sans-serif;
-}
-</style>
-<body>
-<p>$ip</p>
-<p>$host</p>
-<p>$desc</p>
-</body>
-</html>");
+        $desc = '';
+        if (strpos($host, '.mobile.')) {
+            $desc = 'connexion 4G';
+        }
+        if (strpos($host, '.wanadoo.')) {
+            $desc = 'connexion ADSL';
+        }
+        return [
+            'ip'   => $ip,
+            'host' => $host,
+            'desc' => $desc,
+        ];
+    }
+
+    public function routeIpHtm($ext = 'htm')
+    {
+        $response = $this->render('@templates/ip.twig', $this->ipData());
+        $response->setSharedMaxAge(2);
+        $response->setExpires((new DateTime())->modify("+2 seconds"));
+        return $response;
     }
 
     public function routeIpTxt()
     {
-        return new Response($_SERVER['REMOTE_ADDR']);
+        $response = new Response(implode("\n", $this->ipData()));
+        $response->headers->set('Content-Type', 'text/plain');
+        return $response;
     }
 };
