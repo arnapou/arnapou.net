191840fe38954221c766973854de6fd2c37a7bd8
Arnaud Buathier <arnaud@buathier.me>
2020-03-25 13:04:36 +0000

    Update php/ip.php

--- php/ip.php
+++ php/ip.php
@@ -6,10 +6,26 @@ use Symfony\Component\HttpFoundation\Response;
 return new class() extends Controller {
     public function configure(): void
     {
-        $this->addRoute('ip{ext}', [$this, 'routeIP'], 'ip')->setRequirement('ext', '(\.[a-z]+)?');
+        $this->addRoute('ip', [$this, 'routeIpTxt'], 'ip');
+        $this->addRoute('ip.{ext}', [$this, 'routeIpHtm'], 'ip')->setRequirement('ext', '(php|htm|html)');
     }
 
-    public function routeIP($ext)
+    public function routeIpHtm($ext)
+    {
+        $ip   = $_SERVER['REMOTE_ADDR'];
+        $host = gethostbyaddr("$ip");
+        return new Response("<!DOCTYPE html>
+<html>
+<title>$ip</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<body>
+<p>$ip</p>
+<p>$host</p>
+</body>
+</html>");
+    }
+
+    public function routeIpTxt($ext)
     {
         return new Response($_SERVER['REMOTE_ADDR']);
     }
