f8646e49ef75ab6d31d4ba3a95b6936131484a03
Arnaud Buathier <arnaud@buathier.me>
2020-03-25 13:13:13 +0000

    Update php/ip.php

--- php/ip.php
+++ php/ip.php
@@ -10,17 +10,36 @@ return new class() extends Controller {
         $this->addRoute('ip.{ext}', [$this, 'routeIpHtm'], 'ip')->setRequirement('ext', '(php|htm|html)');
     }
 
+    public function ipDescription($ip, $host)
+    {
+        if (strpos($host, '.mobile.')) {
+            return 'connexion 4G';
+        }
+        if (strpos($host, '.wanadoo.')) {
+            return 'connexion ADSL';
+        }
+        return '';
+    }
+
     public function routeIpHtm($ext)
     {
         $ip   = $_SERVER['REMOTE_ADDR'];
         $host = gethostbyaddr("$ip");
+        $desc = $this->ipDescription($ip, $host);
         return new Response("<!DOCTYPE html>
 <html>
 <title>$ip</title>
 <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">
-<body style=\"text-align:center; font-family: Arial, Helvetica, sans-serif\">
+<style>
+body {
+    text-align:center;
+    font-family: Arial, Helvetica, sans-serif;
+}
+</style>
+<body>
 <p>$ip</p>
 <p>$host</p>
+<p>$desc</p>
 </body>
 </html>");
     }
